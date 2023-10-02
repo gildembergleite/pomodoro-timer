fa4f3437d4951bab9dad76a15e69069b02be1b27
Gildemberg Leite <gildembergleite@gmail.com>
2023-10-02 16:11:17 -0300

    feat: section responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,19 @@ import { PiPlayFill } from 'react-icons/pi'
 const robotoMono = Roboto_Mono({ subsets: ['latin'], weight: ['700'] })
 
 export default function Home() {
-  const countStyle = 'text-[10rem] px-4 py-6 rounded-lg leading-none'
+  const countStyle =
+    'text-5xl lg:text-[10rem] px-4 py-6 rounded-lg leading-none'
   return (
-    <main className="flex w-full h-full text-gray-title">
+    <main className="flex w-full h-[calc(100vh-94px)] lg:h-full text-gray-title py-10 lg:py-0">
       <section className="flex flex-col w-full h-full justify-center items-center">
         <form className="font-bold" action="">
-          <div className="flex w-full items-center gap-2">
+          <div className="flex flex-col lg:flex-row w-full items-center gap-2">
             <label className="text-lg" htmlFor="task">
-              Vou trabalhar em
+              Vou trabalhar em:
             </label>
             <input
-              className="flex-1 p-2 text-lg bg-gray-elements placeholder:text-gray-placeholder border-b-2 border-gray-placeholder focus:border-gray-title focus:outline-none"
-              placeholder="nome para o seu projeto"
+              className="flex-1 p-2 text-lg text-center lg:text-left bg-gray-elements placeholder:text-gray-placeholder border-b-2 border-gray-placeholder focus:border-gray-title focus:outline-none"
+              placeholder="nome da atividade"
               list="task-suggestions"
               type="text"
               name="task"
@@ -28,24 +29,26 @@ export default function Home() {
               <option value="Trabalho 3" />
             </datalist>
 
-            <label className="text-lg" htmlFor="minutesAmount">
-              durante
-            </label>
-            <input
-              className="w-20 p-2 text-lg bg-gray-elements border-gray-placeholder focus:border-gray-title border-b-2 focus:outline-none text-center"
-              placeholder="00"
-              max={60}
-              min={5}
-              step={5}
-              type="number"
-              name="minutesAmount"
-              id="minutesAmount"
-            />
+            <div className="flex items-center gap-2">
+              <label className="text-lg" htmlFor="minutesAmount">
+                durante
+              </label>
+              <input
+                className="w-20 p-2 text-lg bg-gray-elements border-gray-placeholder focus:border-gray-title border-b-2 focus:outline-none text-center"
+                placeholder="00"
+                max={60}
+                min={5}
+                step={5}
+                type="number"
+                name="minutesAmount"
+                id="minutesAmount"
+              />
 
-            <span>minutos.</span>
+              <span>minutos.</span>
+            </div>
           </div>
 
-          <div className={`${robotoMono.className} flex py-16 gap-4`}>
+          <div className={`${robotoMono.className} flex py-16 lg:gap-4 gap-2`}>
             <span className={`${countStyle} bg-gray-count`}>0</span>
             <span className={`${countStyle} bg-gray-count`}>0</span>
             <span className={`${countStyle} text-green-default`}>:</span>
